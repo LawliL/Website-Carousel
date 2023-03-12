a9a1ef6c0dff1038e078b129008f63cf23610b24
Arthur Fernandes <72046955+LawliL@users.noreply.github.com>
2023-03-11 23:36:55 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,25 @@
-(function() {
-    const botoesCarrossel = document.querySelectorAll('.botao');
-    const imagens = document.querySelectorAll('.carrosel .imagem');
-    let intervalo = 6000;
-  
-    botoesCarrossel.forEach((botao, indice) => {
-        botao.addEventListener('click', () => {
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imgCarrossel = document.querySelectorAll(".imagem");
+let index = 0;
+let intervalo = setInterval(carrosselAutomatico, 5000);
 
-            desativarBotaoSelecionado();
-  
-            selecionarBotaoCarrosel(botao);
-  
-            esconderImagemAtiva();
-  
-            mostrarImagemDeFundo(indice);
-        })
-    })
-  
-    function mostrarImagemDeFundo(indice) {
-        imagens[indice].classList.add('ativa');
-    }
-  
-    function selecionarBotaoCarrosel(botao) {
-        botao.classList.add('selecionado');
-    }
-  
-    function esconderImagemAtiva() {
-        const imagemAtiva = document.querySelector('.ativa');
-        imagemAtiva.classList.remove('ativa');
-    }
-  
-    function desativarBotaoSelecionado() {
-        const botaoSelecionado = document.querySelector('.selecionado');
-        botaoSelecionado.classList.remove('selecionado');
-    }
-  
-    function passarImagem() {
-      let imagemAtiva = document.querySelector('.carrossel .imagem.ativa');
-      let proximaImagem = imagemAtiva.nextElementSibling;
-  
-      if (!proximaImagem) {
-        proximaImagem = document.querySelector('.carrossel .imagem');
-      }
-  
-      imagemAtiva.classList.remove('ativa');
-      proximaImagem.classList.add('ativa');
-    }
-  
-    setInterval(passarImagem, intervalo);
-  })();
-  
-  
+function mudarImagem(novoindex) {
+    botoesCarrossel[index].classList.remove('selecionado');
+    botoesCarrossel[novoindex].classList.add('selecionado');
+    imgCarrossel[index].classList.remove("ativa");
+    imgCarrossel[novoindex].classList.add("ativa");
+    index = novoindex;
+}
 
+botoesCarrossel.forEach((botao, i) => {
+    botao.addEventListener('click', () => {
+        clearInterval(intervalo);
+        mudarImagem(i);
+        intervalo = setInterval(carrosselAutomatico, 5000);
+    });
+});
 
-  
-  
-  
-
+function carrosselAutomatico() {
+    let novoIndex = (index + 1) % imgCarrossel.length;
+    mudarImagem(novoIndex);
+}
